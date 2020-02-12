114845370ed5d5eb74c0bd0ca75430118b06865e
Andrey <sherlok82@gmail.com>
2020-02-12 13:44:57 +0300

    change second task

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var money = prompt("Ваш бюджет на месяц?");
+var money = +prompt("Ваш бюджет на месяц?");
 var time = prompt("Введите дату в формате YYYY-MM-DD");
 var appData = {
     moneyData: money,
@@ -12,14 +12,28 @@ var appData = {
 
 for (var i = 0; i < 2; i++) {
     var a = prompt("Введите обязательную статью расходов в этом месяце"),
-        b = prompt("Во сколько обойдется?");
+        b = +prompt("Во сколько обойдется?");
     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null &&
         a != '' && b != '' && a.length < 50) {
         console.log('done');
         appData.expenses[a] = b;
+    } else {
+        continue;
     }
 
 
 }
 
-console.log(appData.moneyData / 30);
+appData.moneyPerDay = appData.moneyData / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay <= 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка!");
+}
